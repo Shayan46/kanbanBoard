3e466e366ed313e789cd72998031b96d2237d30d
Shayan46 <98816154+Shayan46@users.noreply.github.com>
2023-12-18 11:51:23 +0530

    lock unlock functionality added

--- kanban board/script.js
+++ kanban board/script.js
@@ -4,6 +4,9 @@ let allPriorityColor = document.querySelectorAll(".priority-color");
 let textArea = document.querySelector(".textarea-cont");
 let mainCont = document.querySelector(".main-cont");
 let removeBtn = document.querySelector(".remove-btn");
+
+
+let color =("red", "blue", "green", "black");
 let modalPriorityColor = "black";
 
 //var uid = new ShortUniqueId();
@@ -59,7 +62,7 @@ for (let i = 0; i < allPriorityColor.length;i++){
         }
         allPriorityColor[i].classList.add("active");
         modalPriorityColor =allPriorityColor[i].classList[1];
-        console.log(modalPriorityColor);
+        //console.log(modalPriorityColor);
     })
 
 
@@ -72,7 +75,8 @@ function createTicket(task){
     ticketCont.setAttribute("class","ticket-cont"); 
     ticketCont.innerHTML = `<div class="ticket-color ${modalPriorityColor}" ></div>
                             <div class="ticket-id">#$afnkjfa</div>
-                            <div class="ticket-area">${task}</div>` ;
+                            <div class="ticket-area">${task}</div>
+                            <div class="lock-unlock"><i class="fa-solid fa-lock"></i></div>` ;
     console.log(ticketCont);
     mainCont.append(ticketCont);
 
@@ -83,4 +87,49 @@ ticketCont.addEventListener("click", function(){
         ticketCont.remove();
 })
 
+
+// handle  priority color change
+
+ let ticketColor = ticketCont.querySelector(".ticket-color");
+
+ ticketColor.addEventListener("click",function(){
+    let currentColor = ticketColor.classList[i];
+    let currentColorIndex = color.findIndex(function(col){
+        return col == currentColor;
+    });
+    // for(let i=0;i<color.length;i++){
+    //     if (color[i]== currentColor){
+    //         currentColorIndex = i;
+    //         break;
+    //     }
+    // }
+    // ["red","blue","green","black"]
+    // [0,1,2,3]
+    let nextColorIndex = (currentColorIndex +1)%color.length;
+    let nextColor = color[nextColorIndex] ;
+    console.log("nextColor");
+    ticketColor.classList.remove(currentColor);
+    ticketColor.classList.add(nextColor);
+
+ })
+
+
+ //handle lock an unlock
+
+ let lockUnlockBtn = ticketCont.querySelector(".lock-unlock i ");
+ let ticketArea =ticketCont.querySelector(".ticket-area");
+
+
+ lockUnlockBtn.addEventListener("click",function(){
+    console.log(lockUnlockBtn);
+    if(lockUnlockBtn.classList.contains("fa-lock")){
+        lockUnlockBtn.classList.remove("fa-lock");
+        lockUnlockBtn.classList.add("fa-lock-open");
+        ticketArea.setAttribute("contenteditable","true" );
+     }
+    else{(lockUnlockBtn.classList.remove("fa-lock-open"))    ;
+        lockUnlockBtn.classList.add("fa-lock");
+        ticketArea.setAttribute("contenteditable","false");
+        }
+    })
 }
